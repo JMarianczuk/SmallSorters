3ee22b5f415e64aee3becac7142b9a776a41ce9a
Unknown <jasper.marianczuk@gmail.com>
2018-11-18 10:33:41 +0100

    Added first implementation for perfomancing

---
+++ Performancing.h
@@ -0,0 +1,24 @@
+﻿// Performancing.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <inttypes.h>
+#include <sys/ioctl.h>
+
+#include <linux/perf_event.h>
+#include <linux/hw_breakpoint.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <string.h>
+#include <asm/unistd.h>
+#include <errno.h>
+#include <stdint.h>
+#include <time.h>
+
+
+// TODO: Reference additional headers your program requires here.
