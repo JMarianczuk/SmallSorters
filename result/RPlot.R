3b73862d95776247818c80ee3b123bf159c180f6
Jasper Marianczuk <jasper.marianczuk@gmail.com>
2019-02-25 16:59:16 +0100

    Plotting generated pdf

--- result/RPlot.R
+++ result/RPlot.R
@@ -14,19 +14,18 @@ options = parse_args(opt_parser)
 
 con <- dbConnect(SQLite(), "small_sorters_result.sqlite")
 
-query <- paste("select (v / n) as cycles, s as sorter from stats where a =", options$array_size, "and c = 0 and s not like '%InRow%'")
+query <- paste("select (v / n) as normalized_value, s as sorter from stats where a =", options$array_size, "and c = 0 and s not like '%InRow%'")
 res <- dbGetQuery(con, query)
 
 array_size_string <- paste(options$array_size)
 if (options$array_size < 10) {
     array_size_string <- paste("0", array_size_string, sep="", collapse="")
 }
-filename <- paste("boxplot-array_size", array_size_string, ".png", sep = "", collapse = "")
-# png(file = filename, width=550, height=750)
+filename <- paste("boxplot-array_size", array_size_string, ".pdf", sep = "", collapse = "")
 
-par(mar=c(22,5,2,1))
-ggplot(res, aes(x=sorter, y=cycles)) +
+thisplot <- ggplot(res, aes(x = sorter, y = normalized_value)) +
+    labs(x = "Sorting algorithm", y = "Cpu cycles needed", title = paste("ArraySize = ", options$array_size, sep = "", collapse = "")) +
     geom_boxplot() +
     coord_flip()
 
-ggsave(filename, width=6, height=4)
+ggsave(filename, thisplot, width=18, height=10, units="cm")
