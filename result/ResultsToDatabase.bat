986f8bc9e11d9afb1d4b17de8d69d80a5bf81a03
Jasper Marianczuk <jasper.marianczuk@gmail.com>
2019-04-12 23:14:38 +0200

    set correct cmake flags with O3

--- result/ResultsToDatabase.bat
+++ result/ResultsToDatabase.bat
@@ -1,3 +1,3 @@
-sqlplot import-data -D sqlite:small_sorters_result.sqlite stats data/output_2019-04-12_11-38-24.txt
+sqlplot import-data -D sqlite:small_sorters_result.sqlite stats data/output_2019-04-12_23-05-19.txt
 sqlite3 small_sorters_result.sqlite < create_index.sql
 sqlite3 small_sorters_result.sqlite < copyToSpecialTables.sql
