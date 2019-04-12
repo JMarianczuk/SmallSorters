8a838daf427d83b5e4d40940ba06e8e0ef297560
Jasper Marianczuk <jasper.marianczuk@gmail.com>
2019-04-12 13:43:03 +0200

    Split sql into multiple parts so the copying to the specific tables can be done independently

--- result/ResultsToDatabase.bat
+++ result/ResultsToDatabase.bat
@@ -1,2 +1,3 @@
-sqlplot import-data -D sqlite:small_sorters_result.sqlite stats data/output_2019-04-04_18-50-23.txt
+sqlplot import-data -D sqlite:small_sorters_result.sqlite stats data/output_2019-04-12_11-38-24.txt
 sqlite3 small_sorters_result.sqlite < create_index.sql
+sqlite3 small_sorters_result.sqlite < copyToSpecialTables.sql
--- result/droptables.sql
+++ result/droptables.sql
@@ -1,7 +1,6 @@
 DROP TABLE "normalSort";
 DROP TABLE "normalSortWithout";
 DROP TABLE "inrowSort";
-DROP TABLE "inrowSortWithout";
 DROP TABLE "sampleSort";
 DROP TABLE "sampleSortWithout";
 DROP TABLE "completeSort";
