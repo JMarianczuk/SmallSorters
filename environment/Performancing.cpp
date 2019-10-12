0945ea40490c8e5d69214a0462eb42b1cbf8ae07
Jasper Marianczuk <jasper.marianczuk@gmail.com>
2019-10-12 20:41:19 +0200

    changed cache perf to only record l1 i cache load misses

--- environment/Performancing.cpp
+++ environment/Performancing.cpp
@@ -37,9 +37,9 @@ void Performancing::SetupPerformanceEventAttribute(PerformanceMetric metric)
 			break;
 		case PerformanceMetric::L1_INSTR_CACHE_MISSES:
 			_performanceEventAttribute.type = PERF_TYPE_HW_CACHE;
-			_performanceEventAttribute.config = PERF_COUNT_HW_CACHE_L1I | (PERF_COUNT_HW_CACHE_OP_READ << 8) | (PERF_COUNT_HW_CACHE_RESULT_ACCESS << 16);
+			_performanceEventAttribute.config = PERF_COUNT_HW_CACHE_L1I | (PERF_COUNT_HW_CACHE_OP_READ << 8) | (PERF_COUNT_HW_CACHE_RESULT_MISS << 16);
 			_performanceChildEventAttribute.type = PERF_TYPE_HW_CACHE;
-			_performanceChildEventAttribute.config = PERF_COUNT_HW_CACHE_L1I | (PERF_COUNT_HW_CACHE_OP_READ << 8) | (PERF_COUNT_HW_CACHE_RESULT_MISS << 16);
+			_performanceChildEventAttribute.config = PERF_COUNT_HW_CACHE_L1I | (PERF_COUNT_HW_CACHE_OP_READ << 8) | (PERF_COUNT_HW_CACHE_RESULT_ACCESS << 16);
 			break;
 		case PerformanceMetric::BRANCH_MISSES:
 			_performanceEventAttribute.config = PERF_COUNT_HW_BRANCH_MISSES;
@@ -63,11 +63,11 @@ Performancing::Performancing(PerformanceMetric metric) {
 	SetupPerformanceEventAttribute(metric);
 	_fileDescriptor = syscall(__NR_perf_event_open, &_performanceEventAttribute, 0, -1, -1, 0);
 	// ioctl(_fileDescriptor, PERF_EVENT_IOC_ID, &_idFirst);
-	if (metric == PerformanceMetric::L1_INSTR_CACHE_MISSES)
-	{
-		_childFileDescriptor = syscall(__NR_perf_event_open, &_performanceChildEventAttribute, 0, -1, _fileDescriptor, 0);
-		// ioctl(_fileDescriptor, PERF_EVENT_IOC_ID, &_idSecond);
-	}
+	// if (metric == PerformanceMetric::L1_INSTR_CACHE_MISSES)
+	// {
+	// 	_childFileDescriptor = syscall(__NR_perf_event_open, &_performanceChildEventAttribute, 0, -1, _fileDescriptor, 0);
+	// 	// ioctl(_fileDescriptor, PERF_EVENT_IOC_ID, &_idSecond);
+	// }
 }
 Performancing::~Performancing() {
 	close(_fileDescriptor);
@@ -117,17 +117,17 @@ unsigned long long ReadTicks()
 void Performancing::StartMeasuring() {
 #ifndef IGNORE_MEASUREMENT
 	ioctl(_fileDescriptor, PERF_EVENT_IOC_RESET, PERF_IOC_FLAG_GROUP);
-	_ticks = ReadTicks();
+	// _ticks = ReadTicks();
 	// _time = std::chrono::steady_clock::now();
 	ioctl(_fileDescriptor, PERF_EVENT_IOC_ENABLE, PERF_IOC_FLAG_GROUP);
 #endif
 }
 void Performancing::StopMeasuring() {
 #ifndef IGNORE_MEASUREMENT
-	auto newTicks = ReadTicks();
+	// auto newTicks = ReadTicks();
     // std::chrono::steady_clock::time_point timeEnd = std::chrono::steady_clock::now();
 	ioctl(_fileDescriptor, PERF_EVENT_IOC_DISABLE, PERF_IOC_FLAG_GROUP);
-	_ticks = newTicks - _ticks;
+	// _ticks = newTicks - _ticks;
 	// _timeSpan = std::chrono::duration_cast<std::chrono::duration<int64_t, std::milli>>(timeEnd - _time);
 #endif
 }
@@ -140,8 +140,8 @@ std::tuple<uint64_t, uint64_t> Performancing::GetValues() {
 	uint64_t childValue;
 	if (_performanceMetric == PerformanceMetric::L1_INSTR_CACHE_MISSES)
 	{
-		_ = read(_childFileDescriptor, _resultBuffer, sizeof(_resultBuffer));
-		childValue = _readFormat->value;
+		// _ = read(_childFileDescriptor, _resultBuffer, sizeof(_resultBuffer));
+		// childValue = _readFormat->value;
 	}
 	else 
 	{
